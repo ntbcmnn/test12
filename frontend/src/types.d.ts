c8a9cd0e3aab467de05093ff9b1cd2f0b52cb4ba
Natalya Botsman <botsmannatashaa@gmail.com>
2025-02-22 15:57:21 +0600

    Added interfaces for Picture

--- frontend/src/types.d.ts
+++ frontend/src/types.d.ts
@@ -1,42 +1,54 @@
 export interface IUser {
-    _id: string;
-    token: string;
-    role: string;
-    displayName: string;
-    googleID?: string;
-    avatar: string | null;
-    email: string;
+  _id: string;
+  token: string;
+  role: string;
+  displayName: string;
+  googleID?: string;
+  avatar: string | null;
+  email: string;
+}
+
+export interface IPicture {
+  _id: string;
+  user: IUser;
+  name: string;
+  image: File | null;
+}
+
+export interface IPictureMutation {
+  name: string;
+  image: File | null;
 }
 
 export interface RegisterResponse {
-    user: IUser;
-    message: string;
+  user: IUser;
+  message: string;
 }
 
 export interface RegisterMutation {
-    email: string;
-    avatar: File | null;
-    displayName: string;
-    password: string;
+  email: string;
+  avatar: File | null;
+  displayName: string;
+  password: string;
 }
 
 export interface LoginMutation {
-    email: string;
-    password: string;
+  email: string;
+  password: string;
 }
 
 export interface ValidationError {
-    errors: {
-        [key: string]: {
-            name: string;
-            message: string;
-        }
-    },
-    message: string;
-    name: string;
-    _message: string;
+  errors: {
+    [key: string]: {
+      name: string;
+      message: string;
+    }
+  },
+  message: string;
+  name: string;
+  _message: string;
 }
 
 export interface GlobalError {
-    error: string;
+  error: string;
 }
